4b0d3c42aae5fdd455985794e3df359c992eb273
BDomzal <basiadomzal@gmail.com>
2024-03-20 13:15:28 +0100

    Back to returning epsilon

--- masserstein/deconv_simplex.py
+++ masserstein/deconv_simplex.py
@@ -564,7 +564,8 @@ def dualdeconv4(exp_sp, thr_sps, penalty, penalty_th, quiet=True, solver=LpSolve
                                     """ % (sum(probs)+sum(abyss)))
 
         return {"probs": probs, "noise_in_components": p0_prime, "trash": abyss, "components_trash": abyss_th, 
-        "fun": lp.value(program.objective)+penalty, 'status': program.status, 'common_horizontal_axis': common_horizontal_axis}
+        "fun": lp.value(program.objective)+penalty, 'status': program.status, 'common_horizontal_axis': common_horizontal_axis,
+                'epsilon': epsilon}
 
 
 def estimate_proportions(spectrum, query, MTD=0.25, MDC=1e-8,
