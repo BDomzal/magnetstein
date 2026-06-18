b6cca3188e2861f5750fc1033c1d8a4598b02dcb
BDomzal <basiadomzal@gmail.com>
2026-06-18 17:32:09 +0200

    Trapezoid update

--- setup.py
+++ setup.py
@@ -153,7 +153,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'scipy', 'pulp', 'tqdm'],
+    install_requires=['numpy>=2.0.0', 'scipy', 'pulp', 'tqdm'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
